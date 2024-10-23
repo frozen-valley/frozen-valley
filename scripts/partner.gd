e476eba0dcf34d6611b4fb9a96e36facc783bf2d
dean-rakic <dr55241@fer.hr>
2024-10-23 14:58:36 +0200

    Inventory, new sprite animation and Tree scene (#5)
    Add inventory, new sprite animation and Tree scene

---
+++ scripts/partner.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+var default_scale_x: float = scale.x
+var move_x: float = 0
+var direction_x: int = 1
+
+var runToPlayer := true
+var isInside := false
+
+@onready var player: Player = get_node("/root/World/Player")
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if (runToPlayer):
+		var direction: Vector2 = global_position.direction_to(player.global_position) 
+		velocity = direction * SPEED
+	
+	move_x = Input.get_axis("ui_left", "ui_right")
+
+	# Flip based on X direction
+	if move_x != 0 && direction_x != sign(move_x):
+		direction_x *= -1
+		scale.x = default_scale_x * -1
+	
+	if velocity != Vector2(0,0):
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
+		
+	move_and_slide()
+
+
+func _on_area_2d_2_body_entered(body):
+	$TimerAssist.start()
+	runToPlayer = false
+	isInside = true
+	
+func _on_area_2d_2_body_exited(body):
+	isInside = false
+	$TimerForWalking.start()
+
+func _on_timer_timeout():
+	if (!isInside):
+		runToPlayer = true;
+
+func _on_timer_assist_timeout():
+	velocity = Vector2(0, 0)
--- scripts/player.gd
+++ scripts/player.gd
@@ -8,9 +8,13 @@ var move_y: float = 0
 var direction_x: int = 1
 var pickup_queue: Array[Pickupable]
 
+@export var inventory: Inventory
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _add_to_inventory(item: Pickupable) -> void:
+	var item_type = item.item
 	item.pick_me_up(self)
-	# Add to inventory...
+	inventory.insert(item_type)
 
 func _pickup() -> void:
 	var item: Pickupable = pickup_queue[0]
@@ -28,6 +32,10 @@ func add_to_pickup_queue(item: Pickupable) -> void:
 func remove_from_pickup_queue(item: Pickupable) -> void:
 	pickup_queue.erase(item)
 
+func _process(_delta: float) -> void:
+	if !pickup_queue.is_empty():
+		_listen_for_pickups()	
+
 func _physics_process(_delta: float) -> void:
 	# Get the input directions
 	move_x = Input.get_axis("ui_left", "ui_right")
@@ -40,10 +48,12 @@ func _physics_process(_delta: float) -> void:
 	if move_x != 0 && direction_x != sign(move_x):
 		direction_x *= -1
 		scale.x = default_scale_x * -1
+	
+	if move != Vector2(0,0):
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
 
 	# Update the velocity
 	velocity = move * SPEED
 	move_and_slide()
-
-	if !pickup_queue.is_empty():
-		_listen_for_pickups()	
