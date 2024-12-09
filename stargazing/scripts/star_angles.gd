6083e76d17a8924d0aa70a283daafdfdb6595251
marinprusac <prusac.marin@gmail.com>
2024-12-09 14:32:30 +0100

    fixed inteconnected star charts

--- stargazing/scripts/star_angles.gd
+++ stargazing/scripts/star_angles.gd
@@ -2,23 +2,23 @@ extends Node
 class_name StarAngles
 
 static var big_dipper_angles: Array[Vector2] = [
-	Vector2(1.270, 0.743),
 	Vector2(1.399, 0.851),
+	Vector2(1.270, 0.743),
 	Vector2(1.563, 0.623),
 	Vector2(1.749, 0.682),
 	Vector2(1.991, 0.646),
 	Vector2(2.214, 0.625),
-	Vector2(2.442, 0.508)
+	Vector2(2.442, 0.508),
 ]
 
 static var little_dipper_angles: Array[Vector2] = [
-	Vector2(2.981, 0.420),
 	Vector2(2.954, 0.494),
+	Vector2(2.981, 0.420),
 	Vector2(3.225, 0.468),
 	Vector2(3.170, 0.524),
 	Vector2(3.360, 0.587),
 	Vector2(3.423, 0.693),
-	Vector2(3.444, 0.778)
+	Vector2(3.444, 0.778),
 ]
 
 static var cassiopeia_angles: Array[Vector2] = [
--- stargazing/star_chart.gd
+++ stargazing/star_chart.gd
@@ -2,7 +2,11 @@ extends Path3D
 
 
 func draw(star_angles: Array[Vector2]) -> void:
-	
+	if len(star_angles) == 7:
+		var first_star = star_angles[3]
+		var dir = Vector3.FORWARD.rotated(Vector3.UP, first_star.x)
+		dir = dir.rotated(dir.rotated(Vector3.UP, -PI/2), first_star.y)
+		curve.add_point(dir*250)
 	for star_angle in star_angles:
 		var dir = Vector3.FORWARD.rotated(Vector3.UP, star_angle.x)
 		dir = dir.rotated(dir.rotated(Vector3.UP, -PI/2), star_angle.y)
