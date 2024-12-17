9a267edfd56dea30ebb13c9a505614e74f4ee4fc
dean-rakic <dr55241@fer.hr>
2024-12-17 18:02:02 +0100

    - only UI folder
    **ONLY UI FOLDER**
    - added start Screen
    - added Pause menu in level 1

--- scenes/area_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body):
-	get_parent().get_node("HouseFront").visible = false
-
-
-func _on_body_exited(body):
-	get_parent().get_node("HouseFront").visible = true
