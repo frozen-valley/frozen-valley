f6257f86218c836235653789b8bd6344a0a22a46
Marin Prusac <prusac.marin@gmail.com>
2025-01-05 22:02:42 +0100

    fixed the layering issues with the pause menu mechanic

--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -4,26 +4,31 @@ func _ready():
 	$AnimationPlayer.play("RESET")
 
 func resume():
-	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	$PauseCanvas.layer = 0
+	get_tree().paused = false
 
 func pause():
-	get_tree().paused = true
 	$AnimationPlayer.play("blur")
+	$PauseCanvas.layer = 20
+	get_tree().paused = true
 
+func deffer_tree(pause_mode = false):
+	get_tree().create_timer(0.1).timeout.connect(func(): set_tree_pause(pause_mode))
+
+func set_tree_pause(pause_mode):
+	get_tree().paused = pause_mode
 	
-func testEsc():
-	if Input.is_action_just_pressed("esc") and !get_tree().paused:
-		pause()
-	elif Input.is_action_just_pressed("esc") and get_tree().paused:
-		resume() 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("esc"):
+		if !get_tree().paused:
+			pause()
+		else:
+			resume()
 
 func _on_resume_pressed():
 	resume()
 
 func _on_quit_pressed():
 	get_tree().quit()
-	
-func _process(delta):
-	testEsc()
