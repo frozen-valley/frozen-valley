6874147357661a6f56cb3bfbffb9e4e7ec1e6006
Marin Prusac <prusac.marin@gmail.com>
2025-01-05 21:08:50 +0100

    new ending dialogue and implemented dialogue trigger upon being closed to an item

--- map_levels/scripts/item.gd
+++ map_levels/scripts/item.gd
@@ -6,6 +6,8 @@ class_name Pickupable
 var follow_player := false
 var player: Player
 
+var seen = false
+
 @export var item: InventoryItem
 
 func _ready():
@@ -30,6 +32,11 @@ func _on_body_entered(body: Node2D) -> void:
 	if (!player_body):
 		return
 	player_body.add_to_pickup_queue(self)
+	
+	if not seen:
+		seen = true
+		Dialogic.Inputs.auto_advance.enabled_forced = true
+		Dialogic.start("little_sister_found_something")
 
 
 func _on_body_exited(body: Node2D) -> void:
