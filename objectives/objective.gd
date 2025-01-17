af6752c1ba8791f5ea1a68579f35044e4b600137
Marin Prusac <prusac.marin@gmail.com>
2025-01-17 20:29:18 +0100

    tweaks on the objectives manager, stargazing objectives

--- objectives/objective.gd
+++ objectives/objective.gd
@@ -2,3 +2,4 @@ extends Resource
 class_name Objective
 
 @export var tasks: Array[String]
+@export var sequential: bool = false
--- objectives/objectives_manager.gd
+++ objectives/objectives_manager.gd
@@ -1,26 +1,43 @@
 extends Control
 class_name ObjectivesManager
 
+signal all_solved
+
 @export var objective: Objective
 @onready var label_preload = preload("res://objectives/ObjectiveLabel.tscn")
-
 @onready var container: VBoxContainer = $ObjectivesContainer
 
 var solved: Array[bool] = []
 var task_labels: Array[Label] = []
 
+var task_count: int:
+	get:
+		return len(objective.tasks)
+
+var solve_count: int:
+	get:
+		return solved.count(true)
 
 func _ready():
+	hide()
 	for task in objective.tasks:
 		solved.append(false)
 		var new_label: Label = label_preload.instantiate()
 		new_label.text = "    - " + task
 		container.add_child(new_label)
 		task_labels.append(new_label)
-		
+		if objective.sequential:
+			new_label.hide()
+	task_labels[0].show()
+
+func start():
+	show()
 
 func solve(index: int = -1):
-	if index == -1:
+	if objective.sequential:
+		if index != -1:
+			printerr("Cannot solve an indexed task in a sequential objective.")
+			return
 		index += 1
 		while index<len(solved) and solved[index]:
 			index += 1
@@ -29,14 +46,20 @@ func solve(index: int = -1):
 			return
 		solved[index] = true
 		task_labels[index].modulate = Color(0.25, 1, 0.25, 0.25)
+		if index+1 < len(task_labels):
+			task_labels[index+1].show()
+		else:
+			all_solved.emit()
 	else:
-		if solved[index]:
+		if index == -1:
+			printerr("Tasks need to be solved by index in a non-sequential objective.")
+		elif solved[index]:
 			printerr("Objective '" + objective.tasks[index] + "' is already solved!")
-			return
 		else:
 			solved[index] = true
 			task_labels[index].modulate = Color(0.25, 1, 0.25, 0.25)
-
+		if solve_count == task_count:
+			all_solved.emit()
 
 func _on_button_pressed() -> void:
 	solve()
