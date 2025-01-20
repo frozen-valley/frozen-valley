0c4e664c9586d6fa44f395a988fda26d2c6bb970
dean-rakic <dr55241@fer.hr>
2025-01-20 21:59:22 +0100

    level 1 redesign
    -redesigned level 1
    - house z index acting wonky

--- map_levels/scripts/house_area.gd
+++ map_levels/scripts/house_area.gd
@@ -4,8 +4,11 @@ extends Area2D
 func _on_body_entered(_body):
 	get_parent().get_node("HouseFront").visible = false
 	get_parent().get_node("HouseRightWall").visible = false
+	get_parent().get_node("HouseBack").z_index = -1
+
 
 
 func _on_body_exited(_body):
 	get_parent().get_node("HouseFront").visible = true
 	get_parent().get_node("HouseRightWall").visible = true
+	get_parent().get_node("HouseBack").z_index = 0
