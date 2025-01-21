0c4fcde6ac81eed7bcb1f3f0e44516b91ca07e41
Erik Kranjec <erik.kranjec8@outlook.com>
2025-01-21 14:12:34 +0100

    Fix z-index on map_level2, fix border, make detectors detect only player

--- map_levels/level 2/crossing_detector_2.gd
+++ map_levels/level 2/crossing_detector_2.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var player = get_parent().get_node("Player")
 
 func _on_body_entered(body):
+	if (body is not Player):
+		return
 	TransitionScreen.transition()
 	await TransitionScreen.on_transition_finished
 	place_player()
